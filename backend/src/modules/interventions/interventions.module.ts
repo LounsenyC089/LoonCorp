2666406a213115b7e6de2cde9ccdf0b97ec83c89
LounsenyC089 <57623329+LounsenyC089@users.noreply.github.com>
2025-11-13 10:22:01 +0000

    Revert "feat: scaffold it management platform"

--- backend/src/modules/interventions/interventions.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { InterventionsService } from './interventions.service';
-import { InterventionsController } from './interventions.controller';
-
-@Module({
-  controllers: [InterventionsController],
-  providers: [InterventionsService],
-})
-export class InterventionsModule {}
